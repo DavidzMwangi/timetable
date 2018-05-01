3be25dcfc6fd09fe825bde9e99a54e4cd937aa6a
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2018-05-01 16:07:30 +0300

    addition of permission and roles library

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,35 @@
 |
 */
 
+//Auth::routes();
+
+//auth routes
+// Authentication Routes...
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+$this->post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset');
+
+
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('index',function (){
     return view('index');
 });
+
+Route::group(['middleware'=>'auth'],function (){
+
+
 Route::get('typography',function (){
     return view('typography');
 });
@@ -175,6 +198,6 @@ Route::get('yandex',function (){
 Route::get('changelogs',function (){
     return view('changelogs');
 });
-Auth::routes();
+});
 
 Route::get('/home', 'HomeController@index')->name('home');
